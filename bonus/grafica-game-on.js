72fada75d13cf5326df68b3d098f6d4ba47108ef
FrancescoViterbo <franc.viterbo@gmail.com>
2020-10-29 11:35:15 +0100

    corretto errore nel loop grafico di creazione div

--- bonus/grafica-game-on.js
+++ bonus/grafica-game-on.js
@@ -1,6 +1,6 @@
 while (play) {
     schermataPrincipale.innerHTML = "";
-    for (i = 0; i < [lista1 + lista2]; i++) {
+    for (i = 0; i < (lista1.length + lista2.length); i++) {
         var div = document.createElement("div");
         div.setAttribute("class", "casellaNascosta");
         schermataPrincipale.appendChild(div);
